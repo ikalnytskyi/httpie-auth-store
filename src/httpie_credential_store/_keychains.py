d61b0b2f2142274a84b76f4e7a34cb398574bff8
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2019-11-24 22:13:09 +0200

    Add 'system' keychain
    System keychain integrates well with your system's primary keychain. For
    instance, on GNOME, it's gnome-keyring, on KDE - kwallet, on macOS -
    macOS keychain, etc. It's safe to store your secrets in system keychain
    because they are usually stored encrypted and decrypted on user log-in
    (hence no user action required).

--- src/httpie_credential_store/_keychains.py
+++ src/httpie_credential_store/_keychains.py
@@ -3,6 +3,8 @@
 import abc
 import subprocess
 
+import keyring
+
 
 class KeychainProvider(metaclass=abc.ABCMeta):
     """Keychain provider interface."""
@@ -22,8 +24,29 @@ class ShellKeychain(KeychainProvider):
 
     name = "shell"
 
-    def get(self, command):
-        return subprocess.check_output(command, shell=True).decode("UTF-8")
+    def get(self, *, command):
+        try:
+            return subprocess.check_output(command, shell=True).decode("UTF-8")
+        except subprocess.CalledProcessError as exc:
+            raise LookupError(f"No secret found: {exc}")
+
+
+class SystemKeychain(KeychainProvider):
+    """Retrieve secret from the system's keychain."""
+
+    name = "system"
+
+    def __init__(self):
+        self._keyring = keyring.get_keyring()
+
+    def get(self, *, service, username):
+        secret = self._keyring.get_password(service, username)
+        if not secret:
+            raise LookupError(
+                f"No secret found for '{service}' service and '{username}' "
+                f"username in '{self.name}' keychain."
+            )
+        return secret
 
 
 _PROVIDERS = {
---
+++ tests/keychains/test_shell.py
@@ -0,0 +1,57 @@
+"""Tests shell keychain provider."""
+
+import os
+
+import pytest
+
+
+@pytest.fixture(scope="function")
+def testkeychain():
+    """Keychain instance under test."""
+
+    # For the same reasons as in tests/test_credential_store.py, all imports
+    # that trigger HTTPie importing must be postponed till one of our fixtures
+    # is evaluated and patched a path to HTTPie configuration.
+    from httpie_credential_store import _keychains
+
+    return _keychains.ShellKeychain()
+
+
+def test_secret_retrieved(testkeychain, tmpdir):
+    """The keychain returns stored secret, no bullshit."""
+
+    secrettxt = tmpdir.join("secret.txt")
+    secrettxt.write_text("p@ss", encoding="UTF-8")
+    assert testkeychain.get(command=f"cat {secrettxt.strpath}") == "p@ss"
+
+
+def test_secret_retrieved_pipe(testkeychain, tmpdir):
+    """The keychain returns stored secret even when pipes are used."""
+
+    secrettxt = tmpdir.join("secret.txt")
+    secrettxt.write_text("p@ss\nextra", encoding="UTF-8")
+
+    command = fr"cat {secrettxt.strpath} | head -n 1 | tr -d {os.linesep!r}"
+    assert testkeychain.get(command=command) == "p@ss"
+
+
+def test_secret_not_found(testkeychain, tmpdir):
+    """LookupError is raised when no secrets are found in the keychain."""
+
+    secrettxt = tmpdir.join("secret.txt")
+
+    with pytest.raises(LookupError) as excinfo:
+        testkeychain.get(command=f"cat {secrettxt.strpath}")
+
+    assert str(excinfo.value) == (
+        f"No secret found: Command 'cat {secrettxt.strpath}' returned "
+        f"non-zero exit status 1."
+    )
+
+
+@pytest.mark.parametrize(
+    ["args", "kwargs"], [pytest.param(["echo p@ss"], {}, id="args")]
+)
+def test_keywords_only_arguments(testkeychain, args, kwargs):
+    with pytest.raises(TypeError):
+        testkeychain.get(*args, **kwargs)
